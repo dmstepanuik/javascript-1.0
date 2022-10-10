32f795da8dc0b522b25af4e7aa7168110bf074eb
dmstepanuik <0669218646@ukr.net>
2022-10-10 18:31:41 +0300

    commit 6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Select } from "./select/select";
 // import "./select/styles.scss";
 
-const select = new Select("#select", {});
-// console.log(select);
+const select = new Select("#select", {
+  placeholder: "Выбери пожалуйста элемент",
+});
+
 window.s = select;
